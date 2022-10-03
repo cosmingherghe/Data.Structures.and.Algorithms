583205a1da6501f6b52054a045b8141149727494
cosmingherghe <cosminlinux@gmail.com>
2022-10-02 21:22:07 -0400

    solved Q2 recursion

--- Recursion.java
+++ Recursion.java
@@ -1,5 +1,14 @@
 public class Recursion {
   
+  // How to calculate power of a positive int using recursion ? 
+  public static int numPow(int n, int pow) {
+    if(pow < 2) {
+      return n;
+    }
+
+    return n * numPow(n, pow -1);
+  }
+
   //Q1: How to find the sum of digits of a positive integer number using recursion?   
   public static int sumIntDigits(int n) {
 
--- Sandbox.java
+++ Sandbox.java
@@ -2,6 +2,8 @@ public class Sandbox
 {
 	public static void main(String[] args) {
 		
+    // How to calculate power of a positive int using recursion ?  
+    // System.out.println( Recursion.numPow(2, 3) );
 
     // How to find the sum of digits of a positive integer number using recursion?   
     // int pozInt = 12340;
